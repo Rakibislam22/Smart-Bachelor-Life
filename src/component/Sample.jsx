815062db5428ae6d6243f36d6544ad66d1f51c3d
Your Name <your.email@example.com>
2026-01-31 18:29:02 +0600

    Nothing

---
+++ src/component/Sample.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sample = () => {
+    return (
+        <div>
+            <h2>Ready for another world!!</h2>
+        </div>
+    );
+};
+
+export default Sample;
