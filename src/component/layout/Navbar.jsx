5c031cf8cdbb5af0680d429b9b9c918092608a52
alifjobaer12 <alifjobaer12@gmail.com>
2026-02-03 20:30:25 +0600

    fix-bug : navbar responsive, mx-w

--- src/component/layout/Navbar.jsx
+++ src/component/layout/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Button from "../common/ButtonPrimary.jsx";
 // eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
-import "../../index.css";
 import ThemeToggle from "../common/ThemeToggle.jsx";
 import Logo from "../common/Logo.jsx";
 import { Menu, X } from "lucide-react";
@@ -18,14 +17,14 @@ const Navbar = () => {
 	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
 	return (
-		<header className="fixed top-0 left-0 right-0 py-5 bg-transparent">
-			<nav className="container flex items-center justify-between px-6 mx-auto sm:block">
+		<header className="fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-md py-5 ">
+			<nav className=" flex items-center justify-between mx-auto sm:block sm:px-10 lg:p-0 max-w-7xl ">
 				<motion.div
 					initial={{ opacity: 0, y: -10 }}
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 1, ease: "easeIn" }}
 				>
-					<div className="flex justify-between pt-5 text-center px-15 ">
+					<div className="flex justify-between text-center px-10 sm:px-0">
 						<div className="flex justify-items-center">
 							<Logo />
 						</div>
@@ -60,7 +59,7 @@ const Navbar = () => {
 					initial={{ opacity: 0, y: -10 }}
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 1, ease: "easeIn" }}
-					className="p-2 cursor-pointer md:hidden text-foreground"
+					className="pt-5 px-10 cursor-pointer md:hidden text-foreground"
 					onClick={() => setIsMobileMenuOpen((prev) => !prev)}
 				>
 					{isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
@@ -80,10 +79,10 @@ const Navbar = () => {
 						filter: "blur(0px)",
 					}}
 					transition={{
-						duration: 0.8,
+						duration: 0.5,
 						ease: "easeOut",
 					}}
-					className="flex flex-col sm:hidden px-9 pb-5 w-full bg-background gap-2.5 justify-items-center rounded-2xl "
+					className=" absolute top-full left-0 flex flex-col sm:hidden px-9 pb-5 w-full bg-background gap-2.5 justify-items-center rounded-2xl "
 				>
 					<div className="flex flex-col gap-10 p-2.5 justify-items-center">
 						{navLinks.map((link, index) => (
