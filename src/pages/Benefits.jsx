79a5c8b2c91705ad1ff96bfe67faff40ff601fe8
Your Name <your.email@example.com>
2026-02-07 16:36:11 +0600

    check benefits

--- src/pages/Benefits.jsx
+++ src/pages/Benefits.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Benefits = () => {
     return (
         <div className='min-h-screen'>
-            <h1>This is the Benefits page</h1>
+            <h1>This is the Benefits page ,  and  jisan will do it . but when ??</h1>
         </div>
     );
 };
