3f61eaf186584b5cbc2643135d097898327afec8
alifjobaer12 <alifjobaer12@gmail.com>
2026-02-07 14:03:18 +0600

    fix layout

--- src/component/common/Card.jsx
+++ src/component/common/Card.jsx
@@ -5,7 +5,7 @@ import Logo from "./Logo.jsx";
 const Card = ({ title, description }) => {
 	return (
 		<div>
-			<div className="flex gap-20 left-5 top-4.25 w-83.5 h-101 flex-col justify-items-center rounded-[25px]  bg-[linear-gradient(101deg,rgba(13,77,56,0.2)_18.53%,rgba(8,136,88,0.2)_81.47%)]  backdrop-blur-[5px]  shadow-[0_10px_30px_-12px_rgba(7,117,80,0.35)] hover:-translate-y-1 hover:shadow-[0_18px_40px_-10px_rgba(7,117,80,0.45)] transition-all duration-300 ">
+			<div className="flex gap-20 left-5 top-4.25 w-83.5 h-101 flex-col justify-between rounded-[25px]  bg-[linear-gradient(101deg,rgba(13,77,56,0.2)_18.53%,rgba(8,136,88,0.2)_81.47%)]  backdrop-blur-[5px]  shadow-[0_10px_30px_-12px_rgba(7,117,80,0.35)] hover:-translate-y-1 hover:shadow-[0_18px_40px_-10px_rgba(7,117,80,0.45)] transition-all duration-300 ">
 				<div className="flex flex-col items-center px-5 text-center gap-7 pt-21 ">
 					<h2 className=" font-urbanist font-medium text-foreground text-[30px] leading-9.5 text-center self-stretch flex-none pb-5">
 						{" "}
@@ -16,7 +16,7 @@ const Card = ({ title, description }) => {
 						{description}{" "}
 					</p>
 				</div>
-				<div className="flex justify-center">
+				<div className="flex justify-center pb-10">
 					<Logo />
 				</div>
 			</div>
