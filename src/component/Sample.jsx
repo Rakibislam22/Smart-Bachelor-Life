6cbb3852f4129a1bd54be529f867a83a9be7b12d
Pranta <prantakumerpandit@gmail.com>
2026-02-11 13:09:54 +0600

    fix

--- src/component/Sample.jsx
+++ src/component/Sample.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Sample = () => {
     return (
         <div>
-            <h2>hellow world!!</h2>
+            <h2>hellow world!! and the owner of joshore is alif fool ! Lol...</h2>
         </div>
     );
 };
