2771a42878f61575bb6011c71e3856ed6b9e30de
alifjobaer12 <alifjobaer12@gmail.com>
2026-02-11 10:24:29 +0600

    add highlight, change img src

--- src/component/landingLayout/FeatureSection.jsx
+++ src/component/landingLayout/FeatureSection.jsx
@@ -1,22 +1,21 @@
 import React from "react";
-// import illustration from "..src/assets/images/landing.png";
-// use your own image path
+import landingImage from "../../assets/images/landing.png";
 
 const FeatureSection = () => {
 	return (
-		<section className="border-2 border-black min-h-dvh">
-			<div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center max-w-6xl p-10 mx-auto pt-35 gap-25 ">
+		<section className="min-h-dvh">
+			<div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center max-w-7xl p-10 mx-auto pt-35 gap-25 ">
 				{/* Left Text */}
 				<div className="">
 					{/* <p className="mb-3 text-sm tracking-wide uppercase text-highlight/90">
 						Eliminate distractions
 					</p> */}
 
-					<h2 className="mb-4 text-3xl font-medium md:text-4xl font-urbanist">
-						Productivity without the chaos
+					<h2 className="mb-4 text-3xl font-medium md:text-[48px] font-urbanist">
+						Productivity <span className="text-primary">Without the Chaos</span>
 					</h2>
 
-					<p className="leading-relaxed text-subtle font-unbounded font-normal">
+					<p className="leading-relaxed text-subtle font-unbounded font-light text-sm sm:text-[15px]">
 						Smart Bachelor Life brings you a place to keep track of
 						your necessary living expenses in your chaotic bachelor
 						life.
@@ -26,7 +25,7 @@ const FeatureSection = () => {
 				{/* Right Image */}
 				<div className="flex justify-center">
 					<img
-						src="src\assets\images\landing.png"
+						src={landingImage}
 						alt="Productivity illustration"
 						className="w-full max-w-sm"
 					/>
