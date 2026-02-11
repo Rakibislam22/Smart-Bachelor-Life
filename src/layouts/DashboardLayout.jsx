98dda81802decc2f7ad2fcee556905507ea8cade
Rakibislam22 <rakibislam13122018@gmail.com>
2026-02-12 01:05:12 +0600

    Add avatar icon and chat icon

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -5,6 +5,8 @@ import { FcBarChart, FcCalendar, FcDebt } from 'react-icons/fc';
 import { TbCoinTakaFilled } from 'react-icons/tb';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import { FaAmazonPay, FaCartPlus } from 'react-icons/fa';
+import Avatar from '../component/Avatar';
+import GroupChatIcon from '../component/GroupChatIcon';
 
 const DashboardLayout = () => {
     return (
@@ -18,7 +20,15 @@ const DashboardLayout = () => {
                             {/* Sidebar toggle icon */}
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-6"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
                         </label>
-                        <div className="px-4"> <Logo></Logo></div>
+                        <div className='flex justify-between items-center w-full px-2'>
+                            <div className="px-4 "> <Logo></Logo></div>
+
+                            <div className='flex justify-center items-center gap-5'>
+                                <GroupChatIcon />
+                                <Avatar />
+                            </div>
+
+                        </div>
                     </nav>
                     {/* Page content here */}
                     <div className="p-4">Page Content</div>
